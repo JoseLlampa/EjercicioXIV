7c6eb175ac9e3c8442f2344ad236113904c0fead
JoseLlampa <62297243+JoseLlampa@users.noreply.github.com>
2020-04-07 10:46:24 -0300

    Se muestra la tabla de multiplicar del numero ingresado.

---
+++ EjercicioXIV/EjercicioXIV/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioXIV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int multiplo;
+            bool error = false;
+
+            do
+            {
+                do
+                {
+                    error = false;
+                    if (!int.TryParse(Console.ReadLine(), out numero))
+                    {
+                        Console.WriteLine("Debe ingresar un numero entero positivo");
+                        error = true;
+                    }
+                } while (error);
+
+                if (numero<0)
+                {
+                    Console.WriteLine("Ingrese un numero entero positivo");
+                }
+            } while (numero < 0);
+            
+            for (int i=1;i<=10;i++)
+            {
+                multiplo = numero * i;
+                Console.WriteLine(numero + " * " + i + " = " + multiplo);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
